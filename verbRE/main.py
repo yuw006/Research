78185b96a8ff7348f3847d6f389e3d51783922ba
yuw006 <63177486+yuw006@users.noreply.github.com>
2021-02-13 23:08:52 -0800

    Update main.py

--- verbRE/main.py
+++ verbRE/main.py
@@ -2,18 +2,18 @@ import spacy
 from Tree import Tree, Node
 nlp = spacy.load("en_core_web_lg")
 i = 19
-#readfile
-rf = open("examples.txt","r")
-tf = open("trees.txt","a")
-f = open("results.txt","a")
+#I commented out all the files to read and write to you can add in your own, or if it's too messy I can do that
+#rf = open("examples.txt","r")
+#tf = open("trees.txt","a")
+#f = open("results.txt","a")
 while i == 19:#i > 0:
     sentence = rf.readline()
     if sentence == "":
         break
-    #doc = nlp( rf.readline().strip("\n"))
+    #this is the sentence to parse
     doc = nlp("After the bell rang, the children came home from school.")
     verbPhrase = rf.readline()
-    #verbPhrase = verbPhrase.strip("\n")
+    #this is the verb to search with
     verbPhrase = "rang"
     tree = None
     root = None
@@ -27,20 +27,6 @@ while i == 19:#i > 0:
             tree = Tree(root,verbPhrase)
             tree.createTree(token,root)
 
-
-#frontier=[root2]
-#print(root2.dep_)
-#print(root2.text)
-#while frontier:
-#    print(' '.join(node.dep_ for node in frontier))
-#    print(' '.join(node.text for node in frontier))
-#    #print(' '.join(node.pos_tag for node in frontier))
-#    next_level = list()
-#   for n in frontier:
-#        for child in n.children:
-#            next_level.append(child)
-#    frontier = next_level
-#print("***********************")
     tf.write(sentence)
     tree.levelTraversal(tf)
 
@@ -51,16 +37,20 @@ while i == 19:#i > 0:
 
     else:
         if tree.growth1(tree.verb):
-            #print 1
-            f.write("growth1 used")
+            #this is for testing feel free to disable
+            #f.write("growth1 used")
+            pass
         if tree.growth2(tree.verb):
-            #print 2
-            f.write("growth2 used")
+            #this is for testing feel free to disable
+            #f.write("growth2 used")
+            pass
         tree.growth3to4(tree.verb)
         if tree.growth5(tree.verb):
-            #print 5
-            f.write("growth5 used")
-        f.write("\n")
+            #this is for testing feel free to disable
+            #f.write("growth5 used")
+            pass
+        
+        #f.write("\n")
         tree.reduction6(tree.verb)
         tree.reduction7(tree.verb)
         tree.reduction8to9(tree.verb)
@@ -69,14 +59,14 @@ while i == 19:#i > 0:
         tree.subj_obj12(tree.verb)
         tree.subj_obj13(tree.verb)
 
-        tree.levelTraversal(tf)
-        f.write("verb: " + verbPhrase)
-        f.write("\n")
-        tree.levelTraversal3(f)
-        tree.levelTraversal2(f)
+        #tree.levelTraversal(tf)
+        #f.write("verb: " + verbPhrase)
+        #f.write("\n")
+        #tree.levelTraversal3(f)
+        #tree.levelTraversal2(f)
     del tree
-    rf.readline()
+    #rf.readline()
     i = i - 1
-rf.close()
-f.close()
-tf.close()
+#rf.close()
+#f.close()
+#tf.close()
